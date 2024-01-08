88fc353e87add53d7e13a60a9d6e4ad2b7836335
Evgeniy Baranenko <arbat2300@gmail.com>
2024-01-08 12:40:54 +0200

    fixed ITower a little

--- DabloonsPP/DabloonsPP/GameObjects/Towers/ITower.cs
+++ DabloonsPP/DabloonsPP/GameObjects/Towers/ITower.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace DabloonsPP
 {
-    //abstract class ITower : IGameObject
-    //{
-    //    public float damage;
-    //    public float range;
+    abstract class ITower : IGameObject
+    {
+        public int damage { get; set; }
+        public float range { get; set; }
 
-    //    protected abstract void Shoot();
-    //}
+        protected abstract void Shoot();
+
+        public ITower(int width, int height, int x, int y, string path, Canvas canva, int damage, float range) : base(width, height, x, y, path, canva)
+        {
+            this.damage = damage;
+            this.range = range;
+        }
+    }
 }
