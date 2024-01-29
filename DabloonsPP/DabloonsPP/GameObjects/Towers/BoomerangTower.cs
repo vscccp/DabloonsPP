8f3f8bad2303894c9a0440ffdee16b19c1e19554
Evgeniy Baranenko <arbat2300@gmail.com>
2024-01-29 11:28:53 +0200

    bruhh

--- DabloonsPP/DabloonsPP/GameObjects/Towers/BoomerangTower.cs
+++ DabloonsPP/DabloonsPP/GameObjects/Towers/BoomerangTower.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace DabloonsPP.GameObjects.Towers
 {
-    internal class BoomerangTower
+    internal class BoomerangTower : ITower
     {
+        private int projectile_speed = 50;
+        private int pierce = 1;
+        private static int width = 75;
+        private static int height = 75;
+        public BoomerangTower(int x, int y, Canvas canva, int damage, List<Bloon> enemies) :
+            base(width, height, (x - (width / 2)), (y - (height / 2)), "Monkeys\\super_monkey.png", canva, damage, 300, enemies, TimeSpan.FromMilliseconds(100))
+        {
+        }
+
+        protected override void Shoot(double angle)
+        {
+            // Calculate the velocity components
+            double speed = projectile_speed;
+            int vx = (int)(speed * Math.Cos(angle));
+            int vy = (int)(speed * Math.Sin(angle));
+
+            Projectile projectile = new Projectile(Position.X, Position.Y, vx, vy, damage, pierce, "Projectiles\\dart.png", (float)angle, GameCanvas, enemies);
+        }
     }
 }
