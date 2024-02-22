04c4d62069b7f7c68b52a4d3ab2ebc14d51b4872
Evgeniy Baranenko <arbat2300@gmail.com>
2024-02-22 09:11:09 +0200

    Created additional classes that represent the tables in the in the database

---
+++ DabloonsPP/Dabloons_Service/Matches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dabloons_Service
+{
+    [DataContract]
+    public class Matches
+    {
+        [DataMember]
+        public int MatchId { get; set; }
+
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public TimeSpan TimeTaken { get; set; }
+
+        [DataMember]
+        public int MoneyAccumulated { get; set; }
+
+        [DataMember]
+        public int TowersBuilt { get; set; }
+
+        [DataMember]
+        public char Result { get; set; }
+    }
+}
---
+++ DabloonsPP/Dabloons_Service/Unlocked.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dabloons_Service
+{
+    [DataContract]
+    public class Unlocked
+    {
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public int GameCurrency { get; set; }
+
+        [DataMember]
+        public int MapsUnlocked { get; set; }
+
+        [DataMember]
+        public int MoneyTiers { get; set; }
+
+        [DataMember]
+        public int HealthTiers { get; set; }
+    }
+}
