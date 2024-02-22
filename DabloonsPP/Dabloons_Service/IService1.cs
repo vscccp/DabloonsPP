1ac9874742a6331aa344b7cd6b17dc6174797c89
Evgeniy Baranenko <arbat2300@gmail.com>
2024-02-22 09:04:48 +0200

    added basic queries for login and getting user

--- DabloonsPP/Dabloons_Service/IService1.cs
+++ DabloonsPP/Dabloons_Service/IService1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -7,39 +8,24 @@ using System.Text;
 
 namespace Dabloons_Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
         [OperationContract]
-        string GetData(int value);
+        bool TryLogin(User user);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        User GetUserByUsername(string username);
 
-        // TODO: Add your service operations here
+        [OperationContract]
+        bool AddUser(User user);
+
+        [OperationContract]
+        List<User> GetAllUsers();
+
+        [OperationContract]
+        bool UpdateUser(User user);
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Dabloons_Service.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    
 }
--- DabloonsPP/Dabloons_Service/Service1.cs
+++ DabloonsPP/Dabloons_Service/Service1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -10,22 +11,131 @@ namespace Dabloons_Service
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        private const string ConnectionString = @"Data Source=CEC-MAGSHIMIM;Initial Catalog=DabloonsDB;Integrated Security=True";
+        private SqlConnection connection = new SqlConnection(ConnectionString);
+
+        public bool AddUser(User user)
         {
-            return string.Format("You entered: {0}", value);
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users (Username, Password) VALUES (@Username, @Password)", connection);
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public User GetUserByUsername(string username)
         {
-            if (composite == null)
+            try
             {
-                throw new ArgumentNullException("composite");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE Username = @Username", connection);
+                cmd.Parameters.AddWithValue("@Username", username);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new User
+                    {
+                        Username = reader["Username"].ToString(),
+                        Password = reader["Password"].ToString()
+                    };
+                }
+                return null;
             }
-            if (composite.BoolValue)
+            catch (Exception e)
             {
-                composite.StringValue += "Suffix";
+                Console.WriteLine(e.Message);
+                return null;
             }
-            return composite;
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Users", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<User> users = new List<User>();
+                while (reader.Read())
+                {
+                    users.Add(new User
+                    {
+                        Username = reader["Username"].ToString(),
+                        Password = reader["Password"].ToString()
+                    });
+                }
+                return users;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool UpdateUser(User user)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Users SET Password = @Password WHERE Username = @Username", connection);
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool TryLogin(User user)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password", connection);
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
