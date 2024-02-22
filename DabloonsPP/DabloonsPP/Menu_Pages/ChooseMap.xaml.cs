75296dc12a32f366926de5ce1678cea8b1c2ac52
Evgeniy Baranenko <arbat2300@gmail.com>
2024-02-22 13:35:58 +0200

    What is going on

--- DabloonsPP/DabloonsPP/Menu_Pages/ChooseMap.xaml.cs
+++ DabloonsPP/DabloonsPP/Menu_Pages/ChooseMap.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace DabloonsPP
+namespace DabloonsPP.Assets.Menu_Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Register : Page
+    public sealed partial class ChooseMap : Page
     {
-        public Register()
+        public ChooseMap()
         {
             this.InitializeComponent();
         }
